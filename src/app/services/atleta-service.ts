3aafca0d04179b4beac71c2283548cfeb015708b
Ronaldo S. <ronaldosm276@gmail.com>
2026-08-13 20:49:17 -0300

    comecei com models e services

---
+++ src/app/services/atleta-service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Pessoa } from '../models/Pessoa';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AtletaService {
+
+  private atletas: Pessoa[] = []
+
+  // sobre private, o que é private
+
+  
+
+}
