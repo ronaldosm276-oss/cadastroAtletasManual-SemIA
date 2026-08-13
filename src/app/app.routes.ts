20043ed0f9dd928cad269bbf4b651f66cbc3a9c4
Ronaldo S. <ronaldosm276@gmail.com>
2026-08-13 20:40:11 -0300

    continuado projeto, ver comments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
 
+import { HomeComponent } from './components/home-component/home-component';
+import { AtletaComponent } from './components/atleta-component/atleta-component';
+import { Teste } from './teste/teste';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        redirectTo:"/home",
+        pathMatch: 'full'
+    }
+,
+    {
+        path: 'home',
+        component:HomeComponent
+
+    }
+,
+    {
+        path: 'cadastroatleta',
+        component:AtletaComponent
+    }
+,
+    {
+        path: 'teste',
+        component:Teste
+    }
+
+];
+// sobre routes, esse path é 'linkDoSite/pathQualquer
+// E chama o componente com component:nomeComponent
 // definir as rotas do componente, mas como funciona?
 // como é a estrutura básica de uma rota?
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './components/menu/menu'
+import { AtletaComponent } from "./components/atleta-component/atleta-component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Menu, AtletaComponent],
+  // você importou o menu lá em import, mas esqueceu de colocar aqui
+  // esse atleta component vc fez de forma automatica, e vai ser inutil
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
